767621cf841443a925b44b56e6bbccbeff2cadc7
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2022-12-08 17:54:30 +0100

    Luke 4 refactor

--- luke4.py
+++ luke4.py
@@ -2,8 +2,7 @@ def convert_to_list(data):
     return [i for i in range(int(data.split('-')[0]), int(data.split('-')[1])+1)]
 
 def read_data():
-    with open('./data/luke4.txt') as f:
-        return [(convert_to_list(x[0]), convert_to_list(x[1])) for x in [x.split(',') for x in [x.strip() for x in open('./data/luke4.txt').readlines()]]]
+    return [(convert_to_list(x[0]), convert_to_list(x[1])) for x in [x.split(',') for x in [x.strip() for x in open('./data/luke4.txt').readlines()]]]
 
 def part1():
     print("Part 1:", sum([1 for x in read_data() if set(x[0]).issuperset(set(x[1])) or set(x[1]).issuperset(set(x[0]))]))
