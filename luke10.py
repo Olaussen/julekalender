764588353bc25147a334424b3530e2a00a56bcd8
Hauk Aleksander Olaussen <olaussen.hauk@gmail.com>
2022-12-10 11:01:04 +0100

    Some refactor

--- luke10.py
+++ luke10.py
@@ -7,26 +7,29 @@ def part1():
     cycle = 0
     signal = 0
 
+    def do_cycle():
+        nonlocal cycle, signal
+        cycle += 1
+        signal += cycle * X if cycle in (20, 60, 100, 140, 180, 220) else 0
+
     for line in read_data():
         match line:
             case ["addx", num]:
                 for _ in range(2):
-                    cycle += 1
-                    signal += cycle * X if cycle in (20, 60, 100, 140, 180, 220) else 0
+                    do_cycle()
                 X += int(num)
             case ["noop"]:
-                cycle += 1
-                signal += cycle * X if cycle in (20, 60, 100, 140, 180, 220) else 0
+                do_cycle()
     print("Part 1:", signal)
 
 def part2():
     X = 1
-    t = 0
+    cycle = 0
 
     def do_cycle():
-        nonlocal t
-        print("#" if t % 40 in (X - 1, X, X + 1) else " ", end="")
-        t += 1; print() if t % 40 == 0 else None
+        nonlocal cycle
+        print("#" if cycle % 40 in (X - 1, X, X + 1) else " ", end="")
+        cycle += 1; print() if cycle % 40 == 0 else None
 
     print("Part 2")
     for line in read_data():
